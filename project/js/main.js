df696ba085597d7dba00ba4fad94e56e46fd49d7
Victoriya <victoriya.brovkina@gmail.com>
2021-02-25 19:12:31 +0300

    Не шмогла я((

--- project/js/main.js
+++ project/js/main.js
@@ -1,36 +1,64 @@
 'use strict'
 
+const API = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses`;
+
+let getRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+            if (xhr.status !== 200) {
+                reject('Error')
+            }
+            resolve(xhr.responseText);
+        }
+        xhr.send();
+    });
+};
+
+getRequest('https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json')
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => console.log(err));
+
+
 class Products {
-    data = [];
     products = [];
     container = null;
 
     constructor(selector) {
         this.container = document.querySelector(selector);
-        this._fetchData();
-        this._render();
-        this.totalCostProducts();
+        this._fetchData()
+            .then(() => this._render())
+            .then(() => this.totalCostProducts());
     }
 
     _fetchData() {
-        this.data = [
-            {title: 'Notebook', id: 1, price: 2000},
-            {title: 'Keyboard', id: 2, price: 200},
-            {title: 'Mouse', id: 3, price: 100},
-            {title: 'Gamepad', id: 4, price: 87}
-        ];
+       return fetch(`${API}/catalogData.json`)
+           .then(result => result.json())
+           .then(data => {
+               for (let product of data) {
+                   this.products.push(new ProductItem(product));
+               }
+           })
     }
 
     _render() {
-        for (let data of this.data) {
-            const product = new ProductItem(data);
-            this.products.push(product);
+        for (let product of this.products) {
+            if (product.rendered) {
+                continue;
+            }
+
             this.container.insertAdjacentHTML('beforeend', product.render());
         }
     }
 
     totalCostProducts() {
-        return  this.products.reduce(((total, item) => total + item.price),0);
+        return this.products.reduce(((total, item) => total + item.price),0);
     }
 }
 
@@ -39,49 +67,76 @@ class ProductItem {
     price = 0;
     id = 0;
     img = '';
+    rendered = false;
 
     constructor(product, img = 'https://via.placeholder.com/250') {
-        ({title: this.title, price: this.price, id: this.id} = product);
+        ({product_name: this.title, price: this.price, id_product: this.id} = product);
         this.img = img;
     }
 
     render() {
+        this.rendered = true;
         return `
         <div class="product-item">
             <img class="product-img" src="${this.img}" alt="${this.title}">
             <div class="product-desc">
                 <h3 class="product-title">${this.title}</h3>
                 <p class="product-price">${this.price}</p>
-                <button class="btn-buy">Buy</button>
+                <button class="btn-buy" data-id="${this.id}">Buy</button>
             </div>
         </div>`
+    }
+
+    addToCart() {
+        document.querySelector('.btn-buy').addEventListener('click', event => {
+  
+        });
     }
 }
 
 class Cart {
-    // cartItems - массив для добавления товаров корзины
-    // container  - свойство для определения класса css в разметке, где будут отрисовываться товары
+    cartItems = [];    
+    container = null;   
 
-    // constructor() - записывает в container переданное значение класса css.
-    // Вызывает метод totalCostCart
+    constructor(selector) {
+        this.container = document.querySelector(selector);
+        //render();
+        //totalCostCart();
 
-    // render() - создает новый объект СartItem и добавляет в массив корзины cartItems.
-    // Вызывает метод addToCart. Вызывается по клику на товар каталога.
-
-    // addToCart() - добавляет в разметку объекты CartItem.
+  }   
+        
+     render() {
+        for (let product of this.cartItems) {
+            this.container.insertAdjacentHTML('beforeend', product.render());
+        }
+     }
 
     // deleteItem() - удаляет объекты CartItem из массива cartItems
 
     // totalCostCart - подсчитывает стоимость корзины.
 }
 
-class CartItem {
-    // наследуется от класса ProductItem
-    // сonstructor - перенимает свойства id, title, price. Создает свойство quantity
+class CartItem extends ProductItem{
+    constructor(product, img = 'https://via.placeholder.com/100') {
+        super(product, img);
+        this.quantity = 1;
+    }
 
-    // render() - возвращает строку с разметкой товара в корзине.
+    render() {
+        this.rendered = true;
+        return `
+        <div class="product-item"> 
+            <img class="product-img" src="${this.img}" alt="${this.title}">
+            <div class="product-desc">
+                <h3 class="product-title">${this.title}</h3>
+                <p class="product-price">${this.price}</p>
+                <button class="btn-buy" data-id="${this.id}">Buy</button>
+            </div>
+        </div>` //ИЗМЕНИТЬ!
+    }
 }
 
 const list = new Products('.products');
+const cart = new Cart('.cart');
 
 
